c8e12d0e201137ed8fa703a0cf25f1cddff9986b
Mona Lisa <HKorosu>
2026-04-12 23:20:05 +0700

    feat(user): user apis and password management

--- src/server.js
+++ src/server.js
@@ -18,10 +18,6 @@ const server = http.createServer(app);
 
 initSocket(server);
 
-// Test Cloudinary
-const cloudinary = require('./config/cloudinary');
-cloudinary.api.ping().then(r => console.log('Cloudinary OK:', r)).catch(e => console.error('Cloudinary Error:', e));
-
 // Bật server lắng nghe trên cổng PORT
 server.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
